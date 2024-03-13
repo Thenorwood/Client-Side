ce33ea3226e4161dc2f5a2257c1285d19e470827
Bryn Norwood <Bryn.Norwood@nscc.ca>
2024-03-13 16:47:55 -0300

    fetched data & store

--- PROG2700_Assignment1C_Cards/Cards.js
+++ PROG2700_Assignment1C_Cards/Cards.js
@@ -1,14 +1,25 @@
-(() =>{
+(function() {
+    function getNewDeck() {
+        fetch('https://deckofcardsapi.com/api/deck/new/')
+        .then(response => response.json()) //change response to json
+        .then(data =>{
+            console.log("Deck:", data); //store deck ID
+            const deckID = data.deck_id;
+
+            localStorage.setItem('deckId', deckId);
+        })
+    //let deckID;
+    //const cardURL = 'https://deckofcardsapi.com';
 //get the tag
 //ids
-const imgTag = document.getElementById();
+//const imgTag = document.getElementById();
 //classes
 const imgTag = document.getElementsByClassName();
 //tag
 
 //modify
-document.getElementsByTagName("img");
-console.log(imgTags)
-imgTags[0].src="https://deckofcardsapi.com/static/img/2D.png";
+//document.getElementsByTagName("img");
+//console.log(imgTags)
+//imgTags[0].src="https://deckofcardsapi.com/static/img/2D.png";
 
 });

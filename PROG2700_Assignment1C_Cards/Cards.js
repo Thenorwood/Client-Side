b4e4fb3b2939b981b151ce1a29fd28b020094f63
Bryn Norwood <Bryn.Norwood@nscc.ca>
2024-03-14 14:25:23 -0300

    edits, cards not displaying

--- PROG2700_Assignment1C_Cards/Cards.js
+++ PROG2700_Assignment1C_Cards/Cards.js
@@ -3,8 +3,13 @@
     let deckId = "";
 
     function getNewDeck() {
-        fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-        .then(response => response.json()) //change response to json
+        fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('networkerror?');
+              }
+              return response.json()
+             }) //change response to json
         .then(data =>{
             deckId = data.deck_id;
            //draw 5 cards
@@ -14,8 +19,13 @@
     }
 
     function drawCards(deckId, count) {
-        fetch("https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${count}")
-        .then(response => response.json())
+        fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=5`)
+        .then(response => {
+            if (!response.ok) {
+              throw new Error('Networkerror again?');
+            }
+            return response.json();
+          })
         .then(data => {
           displayCards(data.cards);
         })
